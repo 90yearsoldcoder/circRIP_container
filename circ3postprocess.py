7ffbea7091bb521c9d4aede914432086afc0e330
“” <minty@bu.edu>
2023-05-16 15:48:42 -0400

    make circ3postprocess.py to handle parameters

--- circ3postprocess.py
+++ circ3postprocess.py
@@ -3,6 +3,7 @@ import errno
 import csv
 import sys
 import shutil
+import argparse
 
 class circ2circRip:
     # copy the result bsj.bed and align files to inputdata folder
@@ -64,6 +65,33 @@ class circ2circRip:
         self.copyAlign()
 
 if __name__ == "__main__":
+    
+    # a typical command: python circ3postprocess.py -p /restricted/projectnb/ncrna/minty/EP4/input/result -n WB_LJ_PS_EX2_33_S33_L001
+    
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Preprocess the cirexplore3 result into CircRIP input format")
+    
+    # Add the arguments
+    parser.add_argument('-p', metavar='path', type=str, help='the path to result folder')
+    parser.add_argument('-n', metavar='name', type=str, help='the name of the sample')
+    
+    # Execute the parse_args() method
+    args = parser.parse_args()
+    
+    print("The path to result file is: ", args.p)
+    print("The name of the file is ", args.n)
+    
+    #excute process
+    
+    folder_path = os.path.join(args.p, args.n)
+    name = args.n
+    Process = circ2circRip(name, folder_path)
+    Process.run()
+    
+    
+    
+    #test block
+    '''
     folder_path_bg  = "/restricted/projectnb/ncrna/minty/EP4/input/result/WB_LJ_PS_EX2_33_S33_L001/"
     name_bg = "WB_LJ_PS_EX2_33_S33_L001"
     bg = circ2circRip(name_bg, folder_path_bg)
@@ -72,3 +100,4 @@ if __name__ == "__main__":
     name_ip = "WB_LJ_PS_EX2_49_S49_L001"
     ip = circ2circRip(name_ip, folder_path_ip)
     ip.run()
+    '''
